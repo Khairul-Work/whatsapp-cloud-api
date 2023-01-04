0347f38470b207cc294ee393619d890d84259756
Khairul-Work <workkhairul989@gmail.com>
2023-01-04 16:10:08 +0800

    fix some other bugs

--- src/webhook/webhook.module.ts
+++ src/webhook/webhook.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { WebhookController } from './webhook.controller';
 import { ConfigService } from '@nestjs/config/dist/config.service';
+import { HttpModule, HttpService } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+  imports:[ConfigModule, HttpModule],
   controllers: [WebhookController],
-  providers: [ConfigService],
 })
 export class WebhookModule {}
